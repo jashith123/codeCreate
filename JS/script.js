f14662e498c8dc84c7d5eaf5b386de34abf222c0
Jashith <jashithnarang@gmail.com>
2024-09-08 17:58:35 +0530

    commited

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+const navbar = document.querySelector('.navbar')
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        navbar.classList.add('sticky')
+    } else {
+    }
+})
